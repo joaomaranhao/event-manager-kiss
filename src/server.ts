ad6d3c15421decb764f95f6977e902c2bfd78bb3
Joao Maranhao <joaofelipemaranhao@gmail.com>
2022-09-30 04:41:12 -0300

    feat: create server

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const port = 3333
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' })
+})
+
+app.listen(port, () => console.log(`[server]: running on http://localhost:${port}`))
