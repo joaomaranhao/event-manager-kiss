7263e0b5c46b5842efc035653132a7ad2a331cbd
João Maranhão <joaofelipemaranhao@gmail.com>
2022-09-30 19:22:42 -0300

    feat: handle error to required param on CreatePersonController

--- src/modules/persons/useCases/createPerson/CreatePersonController.ts
+++ src/modules/persons/useCases/createPerson/CreatePersonController.ts
@@ -3,6 +3,9 @@ import { CreatePersonUseCase } from './CreatePersonUseCase'
 
 export class CreatePersonController {
   async handle (request: Request, response: Response) {
+    if (!request.body.name) {
+      throw new Error('Name is required')
+    }
     const { name } = request.body
 
     const createPersonUseCase = new CreatePersonUseCase()
