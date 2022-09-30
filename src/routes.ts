5308971cc1b6e7b4eea7ffaeb8e881f67534b777
Joao Maranhao <joaofelipemaranhao@gmail.com>
2022-09-30 14:18:36 -0300

    feat: create CreateUser route

---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+import { CreatePersonController } from './modules/persons/useCases/createPerson/CreatePersonController'
+
+export const routes = Router()
+
+const createPersonController = new CreatePersonController()
+
+routes.post('/persons', createPersonController.handle)
